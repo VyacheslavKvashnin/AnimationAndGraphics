c57538db145f79def102ecc24c06f32cf8d16571
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-26 10:53:11 +0500

    Initial Commit

---
+++ AnimationAndGraphics/AnimationAndGraphicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAndGraphicsApp.swift
+//  AnimationAndGraphics
+//
+//  Created by Вячеслав Квашнин on 26.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAndGraphicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
