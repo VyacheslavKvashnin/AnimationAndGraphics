99fa5e7f7e0676ea7feaec718fc38507fffe858f
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-26 20:44:42 +0500

    Ending project

--- AnimationAndGraphics/ContentView.swift
+++ AnimationAndGraphics/ContentView.swift
@@ -8,10 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var startAnimation = false
+    @State private var angle = 0.0
+    
     var body: some View {
         ZStack {
-            ShipView()
-        }
+            VStack {
+                Button {
+                    startAnimation.toggle()
+                    angle = 5.0
+                } label: {
+                    Text("Show")
+                        .padding()
+                        .background(.blue)
+                        .cornerRadius(15)
+                        .frame(width: 200, height: 50)
+                        .foregroundColor(.white)
+                }
+                
+                ShipView()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .offset(y: 350)
+                    .rotationEffect(.degrees(angle))
+                    .animation(.linear(duration: 2).repeatForever(autoreverses: true), value: angle)
+                Spacer()
+                ZStack {
+                    WaveView(yOffset: startAnimation ? 0.5 : -0.5)
+                        .fill(.blue)
+                        .shadow(radius: 8)
+                        .frame(height: 190)
+                        .opacity(0.3)
+                        .rotationEffect(.degrees(180))
+                        .animation(.easeInOut(duration: 4).repeatForever(autoreverses: true), value: startAnimation)
+                    
+                    WaveView(yOffset: startAnimation ? -0.3 : 0.3)
+                        .fill(.blue)
+                        .shadow(radius: 8)
+                        .frame(height: 230)
+                        .opacity(0.2)
+                        .rotationEffect(.degrees(180))
+                        .animation(.easeInOut(duration: 3).repeatForever(autoreverses: true), value: startAnimation)
+                    
+                    WaveView(yOffset: startAnimation ? -0.6 : 0.6)
+                        .fill(.blue)
+                        .shadow(radius: 8)
+                        .frame(height: 170)
+                        .opacity(0.4)
+                        .padding(.horizontal, -80)
+                        .rotationEffect(.degrees(180))
+                        .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true), value: startAnimation)
+                }
+            }
+        }.edgesIgnoringSafeArea(.bottom)
     }
 }
 
--- AnimationAndGraphics/ShipView.swift
+++ AnimationAndGraphics/ShipView.swift
@@ -14,6 +14,7 @@ struct ShipView: View {
             let width = geometry.size.width
             let hight = geometry.size.height
             let size = min(width, hight)
+            let middle = size / 2
             
             // корпус
             Path { path in
@@ -21,8 +22,9 @@ struct ShipView: View {
                 path.addLine(to: CGPoint(x: size * 0.9, y: size * 0.65))
                 path.addLine(to: CGPoint(x: size * 0.8, y: size * 0.9))
                 path.addLine(to: CGPoint(x: size * 0.4, y: size * 0.9))
+                path.closeSubpath()
             }
-            .fill(Color.purple)
+            .fill(.purple)
             
             // штанга
             Path { path in
@@ -30,22 +32,25 @@ struct ShipView: View {
                 path.addLine(to: CGPoint(x: size * 0.55, y: 0))
                 path.addLine(to: CGPoint(x: size * 0.55, y: size * 0.65))
                 path.addLine(to: CGPoint(x: size * 0.53, y: size * 0.65))
+                path.closeSubpath()
             }
-            .fill(Color.gray)
+            .fill(.gray)
             
             // передний парус
             Path { path in
-                path.move(to: CGPoint(x: size * 0.5, y: size * 0.1))
+                path.move(to: CGPoint(x: middle, y: size * 0.1))
                 path.addLine(to: CGPoint(x: size * 0.5, y: size * 0.60))
                 path.addLine(to: CGPoint(x: size * 0.15, y: size * 0.60))
+                path.closeSubpath()
             }
-            .fill(Color.yellow)
+            .fill(.yellow)
             
             // задний парус
             Path { path in
                 path.move(to: CGPoint(x: size * 0.575, y: size * 0.15))
-                path.addLine(to: CGPoint(x: size * 0.75, y: size * 0.5))
+                path.addLine(to: CGPoint(x: size * 0.75, y: middle))
                 path.addLine(to: CGPoint(x: size * 0.575, y: size * 0.60))
+                path.closeSubpath()
             }
             .fill(.yellow)
             
@@ -54,6 +59,7 @@ struct ShipView: View {
                 path.move(to: CGPoint(x: size * 0.575, y: size * 0.01))
                 path.addLine(to: CGPoint(x: size * 0.8, y: size * 0.055))
                 path.addLine(to: CGPoint(x: size * 0.575, y: size * 0.1))
+                path.closeSubpath()
             }
             .fill(.red)
         }
