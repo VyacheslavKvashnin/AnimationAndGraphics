f56a3adb34da674279b6d7cfcb0e9afe055c1034
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-26 12:20:38 +0500

    Add ship

--- AnimationAndGraphics/ShipView.swift
+++ AnimationAndGraphics/ShipView.swift
@@ -10,45 +10,50 @@ import SwiftUI
 struct ShipView: View {
     var body: some View {
         GeometryReader { geometry in
+            
+            let width = geometry.size.width
+            let hight = geometry.size.height
+            let size = min(width, hight)
+            
             // корпус
             Path { path in
-                path.move(to: CGPoint(x: 10, y: 130))
-                path.addLine(to: CGPoint(x: 180, y: 130))
-                path.addLine(to: CGPoint(x: 160, y: 180))
-                path.addLine(to: CGPoint(x: 80, y: 180))
+                path.move(to: CGPoint(x: size * 0.05, y: size * 0.65))
+                path.addLine(to: CGPoint(x: size * 0.9, y: size * 0.65))
+                path.addLine(to: CGPoint(x: size * 0.8, y: size * 0.9))
+                path.addLine(to: CGPoint(x: size * 0.4, y: size * 0.9))
             }
             .fill(Color.purple)
             
             // штанга
             Path { path in
-                path.move(to: CGPoint(x: 105, y: 0))
-                path.addLine(to: CGPoint(x: 110, y: 0))
-                path.addLine(to: CGPoint(x: 110, y: 130))
-                path.addLine(to: CGPoint(x: 105, y: 130))
+                path.move(to: CGPoint(x: size * 0.53, y: 0))
+                path.addLine(to: CGPoint(x: size * 0.55, y: 0))
+                path.addLine(to: CGPoint(x: size * 0.55, y: size * 0.65))
+                path.addLine(to: CGPoint(x: size * 0.53, y: size * 0.65))
             }
             .fill(Color.gray)
             
             // передний парус
             Path { path in
-                path.move(to: CGPoint(x: 100, y: 20))
-                path.addLine(to: CGPoint(x: 100, y: 120))
-                path.addLine(to: CGPoint(x: 30, y: 120))
+                path.move(to: CGPoint(x: size * 0.5, y: size * 0.1))
+                path.addLine(to: CGPoint(x: size * 0.5, y: size * 0.60))
+                path.addLine(to: CGPoint(x: size * 0.15, y: size * 0.60))
             }
             .fill(Color.yellow)
             
             // задний парус
             Path { path in
-                path.move(to: CGPoint(x: 115, y: 30))
-                path.addLine(to: CGPoint(x: 150, y: 100))
-                path.addLine(to: CGPoint(x: 115, y: 120))
+                path.move(to: CGPoint(x: size * 0.575, y: size * 0.15))
+                path.addLine(to: CGPoint(x: size * 0.75, y: size * 0.5))
+                path.addLine(to: CGPoint(x: size * 0.575, y: size * 0.60))
             }
             .fill(.yellow)
             
             // флаг
             Path { path in
-                path.move(to: CGPoint(x: 115, y: 5))
-                path.addLine(to: CGPoint(x: 160, y: 15))
-                path.addLine(to: CGPoint(x: 115, y: 20))
+                path.move(to: CGPoint(x: size * 0.575, y: size * 0.01))
+                path.addLine(to: CGPoint(x: size * 0.8, y: size * 0.055))
+                path.addLine(to: CGPoint(x: size * 0.575, y: size * 0.1))
             }
             .fill(.red)
         }
---
+++ AnimationAndGraphics/WaveView.swift
@@ -0,0 +1,20 @@
+//
+//  WaveView.swift
+//  AnimationAndGraphics
+//
+//  Created by Вячеслав Квашнин on 26.04.2022.
+//
+
+import SwiftUI
+
+struct WaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WaveView_Previews: PreviewProvider {
+    static var previews: some View {
+        WaveView()
+    }
+}
